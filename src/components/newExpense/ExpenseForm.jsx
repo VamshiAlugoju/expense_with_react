a8ff98236479b2e6b3c9e4ed78acb0e5630b0095
vamshi <alugojuvamshi@gmail.com>
2023-06-25 20:23:28 +0530

    added form

--- src/components/newExpense/ExpenseForm.jsx
+++ src/components/newExpense/ExpenseForm.jsx
@@ -1,21 +1,46 @@
-import React from 'react'
+import React , {useState} from 'react'
 import "./ExpenseForm.css"
 
 function ExpenseForm() {
+
+ const [title , settitle] = useState("");
+ const [amount , setamount] = useState("");
+ const [date , setdate] = useState("");
+
+  function handletitle(e)
+  {
+       settitle(e.target.value);
+  }
+  function handleamount(e)
+  {
+       setamount(e.target.value);
+  }
+  function handledate(e)
+  {
+       setdate(e.target.value);
+  }
+
+  function handlesubmit(e)
+  {
+      e.preventDefault();
+      console.log("clicked");
+  }
+
+
   return (
-      <form action="">
+      <form action="" onSubmit={handlesubmit}>
         <div className='new-expense__controls'>
             <div className="new-expense__control">
                 <label>Title</label>
-                <input type="text" />
+                <input onChange={handletitle} type="text" />
             </div>
             <div className="new-expense__control">
                 <label>Amount</label>
-                <input type="number" min="0.01" step="0.01" />
+                <input onChange={handleamount} type="number" min="0.01" step="0.01" />
             </div>
             <div className="new-expense__control">
                 <label>Date</label>
-                <input type="Date" />
+                <input onChange={handledate} type="Date" />
             </div>
           <div className="new-expense__actions">
             <button type='submit'> Submit </button>
